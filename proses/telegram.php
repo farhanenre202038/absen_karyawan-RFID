2b16db4dbe84a5e30e7b3b638ecc02bc2fe161fe
farhanenre202038 <116834710+farhanenre202038@users.noreply.github.com>
2024-01-06 00:52:42 +0800

    Update telegram.php

--- proses/telegram.php
+++ proses/telegram.php
@@ -1,11 +1,8 @@
 <?php
-    // $telegram_id = 804116558;
-    // $message_datang = 'Selamat Datang, Farhan Waktu Kedatangan Mu 12:20:20';
-    // $secret_token = '5712846332:AAH4pGV0iRkVDkwS1WqEcUB5KyoIaDZSvwc';
     sendMessage($message_datang);
 
     function sendMessage($message_datang) {
-        $url = "https://api.telegram.org/bot5712846332:AAH4pGV0iRkVDkwS1WqEcUB5KyoIaDZSvwc/sendMessage?chat_id=804116558&text=" .$message_datang;
+        $url = "API_KEY" .$message_datang;
         $ch = curl_init();
         $optarray = array(
             CURLOPT_URL => $url,
